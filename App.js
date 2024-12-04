781ffa271b246870040a7de3a62d8d3d0a930b57
gusdiaso <gustavou.dias@gmail.com>
2024-12-04 00:53:50 -0300

    finaliza app

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Alert } from 'react-native';
-import Login from "./telas/login/login"
+import { Alert, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, View } from 'react-native';
+import Login from "./telas/login/login";
 import Home from './telas/home/home';
 import CadastroFuncionario from './telas/cadastroFuncionarios/cadastrofuncionarios';
 import AddFuncionarios from './telas/addFuncionario/addfuncionario';
@@ -26,7 +26,7 @@ export default function App() {
   useEffect(() => {
     const loadData = async () => {
       try {
-        // await AsyncStorage.clear();//////
+        // await AsyncStorage.clear(); // Uncomment for debugging purposes
 
         const existingUserData = await AsyncStorage.getItem('users');
         if (!existingUserData) {
@@ -61,102 +61,100 @@ export default function App() {
   }, []);
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
+          <NavigationContainer>
+            <Stack.Navigator>
 
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
+              <Stack.Screen
+                name="Login"
+                component={Login}
+                options={{ headerShown: false }}
+              />
 
-        <Stack.Screen 
-          name="Home" 
-          component={Home} 
-          options={{ headerBackVisible: false }}
-        />
+              <Stack.Screen 
+                name="Home" 
+                component={Home} 
+                options={{ headerBackVisible: false }}
+              />
 
-        <Stack.Screen 
-          name="Cadastrar Funcionario" 
-          component={CadastroFuncionario} 
-          options={{ }}
-        />
+              <Stack.Screen 
+                name="Cadastrar Funcionario" 
+                component={CadastroFuncionario} 
+                options={{ }}
+              />
 
-        <Stack.Screen 
-          name="Adicionar Funcionario" 
-          component={AddFuncionarios} 
-          options={{ }}
-        />
+              <Stack.Screen 
+                name="Adicionar Funcionario" 
+                component={AddFuncionarios} 
+                options={{ }}
+              />
 
-        <Stack.Screen 
-          name="Atualizar Funcionario" 
-          component={UpdateFuncionario} 
-          options={{ }}
-        />
+              <Stack.Screen 
+                name="Atualizar Funcionario" 
+                component={UpdateFuncionario} 
+                options={{ }}
+              />
 
-        <Stack.Screen 
-          name="Remover Funcionario" 
-          component={ApagarFuncionario} 
-          options={{ }}
-        />
+              <Stack.Screen 
+                name="Remover Funcionario" 
+                component={ApagarFuncionario} 
+                options={{ }}
+              />
 
-        <Stack.Screen 
-          name="Listar Funcionarios" 
-          component={ListaFuncionario} 
-          options={{ }}
-        />
+              <Stack.Screen 
+                name="Listar Funcionarios" 
+                component={ListaFuncionario} 
+                options={{ }}
+              />
 
-        <Stack.Screen 
-          name="Busca de Ferramentas" 
-          component={BuscaFerramenta} 
-          options={{ }}
-        />
+              <Stack.Screen 
+                name="Busca de Ferramentas" 
+                component={BuscaFerramenta} 
+                options={{ }}
+              />
 
-        <Stack.Screen 
-          name="Detalhamento da Ferramenta" 
-          component={FerramentaDetalhe} 
-          options={{ }}
-        />
+              <Stack.Screen 
+                name="Detalhamento da Ferramenta" 
+                component={FerramentaDetalhe} 
+                options={{ }}
+              />
 
-        <Stack.Screen 
-          name="Manual da Ferramenta" 
-          component={ManualFerramenta} 
-          options={{ }}
-        />
+              <Stack.Screen 
+                name="Manual da Ferramenta" 
+                component={ManualFerramenta} 
+                options={{ }}
+              />
 
-        <Stack.Screen 
-          name="Emprestimo de Ferramenta" 
-          component={EmprestarFerramenta} 
-          options={{ }}
-        />
-        
-        <Stack.Screen 
-          name="Cadastrar Ferramenta" 
-          component={CadastroFerramentas} 
-          options={{ }}
-        />
-        
-        <Stack.Screen 
-          name="Adicionar Ferramenta" 
-          component={AddFerramentas} 
-          options={{ }}
-        />
-        
-        <Stack.Screen 
-          name="Atualizar Ferramenta" 
-          component={UpadateFerramentas} 
-          options={{ }}
-        />
+              <Stack.Screen 
+                name="Emprestimo de Ferramenta" 
+                component={EmprestarFerramenta} 
+                options={{ }}
+              />
+              
+              <Stack.Screen 
+                name="Cadastrar Ferramenta" 
+                component={CadastroFerramentas} 
+                options={{ }}
+              />
+              
+              <Stack.Screen 
+                name="Adicionar Ferramenta" 
+                component={AddFerramentas} 
+                options={{ }}
+              />
+              
+              <Stack.Screen 
+                name="Atualizar Ferramenta" 
+                component={UpadateFerramentas} 
+                options={{ }}
+              />
 
-        <Stack.Screen 
-          name="Remover Ferramenta" 
-          component={RemoveFerramentas} 
-          options={{ }}
-        />
+              <Stack.Screen 
+                name="Remover Ferramenta" 
+                component={RemoveFerramentas} 
+                options={{ }}
+              />
 
-
-      </Stack.Navigator>
-
-    </NavigationContainer>
+            </Stack.Navigator>
+          </NavigationContainer>
   );
 }
