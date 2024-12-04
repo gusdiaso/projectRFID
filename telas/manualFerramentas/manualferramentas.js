4a147fd37f6df12c4709715d7b0e398eead01a9f
gusdiaso <gustavou.dias@gmail.com>
2024-12-03 23:57:32 -0300

    funcionando

--- telas/manualFerramentas/manualferramentas.js
+++ telas/manualFerramentas/manualferramentas.js
@@ -2,13 +2,13 @@ import { Container, Texto } from './style.js';
 import { useState, useEffect } from 'react';
 import * as Linking from 'expo-linking';
 import { Alert, ActivityIndicator } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
 import axios from 'axios';
-import { useIsFocused } from '@react-navigation/native'; // Importa o hook
+
+
 
 export default function ManualFerramenta({ navigation }) {
   const [tag, setTag] = useState('nenhuma');
-  const isFocused = useIsFocused(); // Verifica se a tela está em foco
 
   // Função para pegar a tag do servidor
   const fetchTag = async () => {
@@ -37,11 +37,13 @@ export default function ManualFerramenta({ navigation }) {
       const response = await axios.get(url, { maxRedirects: 5 });
       return response.request.responseURL || url;
     } catch (error) {
+      
       return url;
     }
   };
-
+  
   const openLink = async (id) => {
+    
     const tools = await AsyncStorage.getItem('tools');
     const toolsArray = tools ? JSON.parse(tools) : [];
     const tool = toolsArray.find(item => item.id === id);
@@ -66,29 +68,29 @@ export default function ManualFerramenta({ navigation }) {
     } else {
       Alert.alert('Erro', 'ID da ferramenta não encontrado ou sem manual associado.');
     }
-
+    
     // Após abrir o manual, setamos a tag como "nenhuma" novamente
     await axios.post('https://new-turtle-civil.ngrok-free.app/dados', { tag: 'nenhuma' });
-
+    
+    
     navigation.navigate("Home");
+    
   };
-
+  
   // Faz a requisição inicial e começa a verificação a cada 1 segundo
   useEffect(() => {
-    if (isFocused) {
-      fetchTag(); // Pega a tag inicialmente
-      const intervalId = setInterval(() => {
-        fetchTag(); // Verifica a tag a cada 1 segundo
-      }, 1000);
+    fetchTag(); // Pega a tag inicialmente
+    const intervalId = setInterval(() => {
+      fetchTag(); // Verifica a tag a cada 1 segundo
+    }, 1000);
 
-      // Limpa o intervalo quando o componente sair do foco
-      return () => clearInterval(intervalId);
-    }
-  }, [isFocused]); // O efeito só roda quando o foco da tela muda
+    // Limpa o intervalo quando o componente for desmontado
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <Container>
-      <ActivityIndicator size="large" color="#000" style={{ transform: [{ scale: 1.5 }] }} />
+      <ActivityIndicator size="large" color="#000" style={{transform: [{scale:1.5}]}}/>
       <Texto>Aproxime a ferramenta do leitor para consultar o manual.</Texto>
     </Container>
   );
